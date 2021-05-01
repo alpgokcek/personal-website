6e7e58ee367cfcf2db4210fac17d58b8b7d72cac
alpgokcek <alp.gokcek1@gmail.com>
2021-05-01 15:58:43 +0300

    cv path updated.

--- pages/cv.js
+++ pages/cv.js
@@ -2,30 +2,27 @@ import React from 'react';
 import BaseLayout from "../components/layouts/BaseLayout"
 import BasePage from "../components/layouts/BasePage"
 import {Col, Container, Row} from "reactstrap";
-class CV extends React.Component {
 
+export default function CV() {
+    const pdfPath = "http://www.alpgokcek.com/static/files/cv.pdf"
 
-    render() {
-        return (
-            <BaseLayout>
-                <BasePage>
-                    <h1 className="page-title">My CV</h1>
-                    <div className="background-image">
-                        <img src="/static/images/background-index.png"/>
-                    </div>
-                    <Container className="cv-page-margin">
-                        <Row>
-                            <Col>
-                            <div style={{ height: '900px' }}>
-                            <iframe style={{ width: '100%', height: "100%" }} src={"http://www.alpgokcek.com/static/files/cv.pdf"}></iframe>
-                            </div>
-                            </Col>
-                        </Row>
-                    </Container>
-                </BasePage>
-            </BaseLayout>
-        );
-    }
+    return (
+        <BaseLayout>
+            <BasePage>
+                <h1 className="page-title">My CV</h1>
+                <div className="background-image">
+                    <img src="/static/images/background-index.png"/>
+                </div>
+                <Container className="cv-page-margin">
+                    <Row>
+                        <Col>
+                        <div style={{ height: '900px' }}>
+                        <iframe style={{ width: '100%', height: "100%" }} src={"http://docs.google.com/viewer?url=" + pdfPath + "&embedded=true"} frameBorder="0" scrolling="no"/>
+                        </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </BasePage>
+        </BaseLayout>
+    );
 }
-
-export default CV;
